46720885aa81de1467fd569b862c55a95fc0654a
JustAnOtherCoder13 <justanothercoder13@gmail.com>
2021-02-11 12:36:33 +0100

    test network

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/NetworkConnectionTest.java
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/NetworkConnectionTest.java
@@ -1,15 +1,23 @@
 package com.openclassrooms.realestatemanager;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static com.openclassrooms.realestatemanager.presentation.utils.Utils.isInternetAvailable;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 @LargeTest
@@ -22,5 +30,22 @@ public class NetworkConnectionTest {
     @Test
     public void testConnectivityWhenAllEnable() {
         assertTrue(isInternetAvailable(ApplicationProvider.getApplicationContext()));
+    }
+
+    @Test
+    public void isWifiEnabled() {
+        WifiManager wifiManager = (WifiManager) ApplicationProvider.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        assert wifiManager != null;
+        wifiManager.setWifiEnabled(false);
+        wifiManager.disconnect();
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                ApplicationProvider.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert connMgr != null;
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        assert networkInfo != null;
+        assertFalse(wifiManager.isWifiEnabled());
+        //assertNotEquals(networkInfo.getType(), ConnectivityManager.TYPE_WIFI);
     }
 }
